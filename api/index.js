6d8147c93e1e1b04d6bb9c36b77b67a49e8b954a
Cesar <CesarCordova.developer@gmail.com>
2023-09-13 20:10:59 -0700

    se preparo el proyecto para el despliegue en vercel5

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 // routes
-app.use('/api/v1', router)
+app.use('api/v1', router)
 
 // static files
 app.get('/api', (req, res) => {
